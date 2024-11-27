0abbada80ea665b1420ab72169e062a1e2eefe62
abhigavande <abhigavande2804@gmail.com>
2024-11-27 12:44:12 +0900

    aaa

---
+++ components/Hero.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+
+import { CustomButton } from "@components";
+
+const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">車探し、予約、レンタルをもっと簡単に！</h1>
+
+        <p className="hero__subtitle">
+          予約が簡単だから、車レンタルもスムーズに！
+        </p>
+
+        <CustomButton
+          title="車を探す"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
